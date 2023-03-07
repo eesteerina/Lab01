51c992290658a7ca429e8d81a3dc405a20088399
esterandreetto <esterandreetto@localhost>
2023-03-07 13:11:43 +0100

    Inizio

--- src/main/java/it/polito/tdp/Lab01/model/Parole.java
+++ src/main/java/it/polito/tdp/Lab01/model/Parole.java
@@ -1,24 +1,34 @@
 package it.polito.tdp.Lab01.model;
 
 import java.util.List;
+import java.util.*;
 
 public class Parole {
+	
+	private List<String> listaParole;
+	private TreeMap<String, String> mappaParole;
 		
 	public Parole() {
-		//TODO
+		 listaParole = new LinkedList<String>();
 	}
 	
 	public void addParola(String p) {
-		//TODO
+		listaParole.add(p);
 	}
 	
 	public List<String> getElenco() {
-		//TODO
-		return null;
+		mappaParole = new TreeMap<String, String>();
+		
+		for(String s : listaParole) {
+			mappaParole.put(s, s);
+		}
+		LinkedList<String> ritorno = new LinkedList(mappaParole.values());
+		
+		return ritorno;
 	}
 	
 	public void reset() {
-		// TODO
+		listaParole.clear();
 	}
 
 }
